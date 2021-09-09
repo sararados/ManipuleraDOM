bf4bbff5f2c42ba7e139a412f0947be09cefecd4
Sara Rados <sararados@hotmail.com>
2021-09-09 09:42:17 +0200

    manipuleraDOM

---
+++ script.js
@@ -0,0 +1,14 @@
+
+showMessage("Sara")
+
+function showMessage (namn){
+
+    document.getElementById('namn').textContent = namn;
+}
+
+
+const thingsImGoodAt=[ "baka","rita","äta","tvätta",]; //array
+
+  
+
+console.log(thingsImGoodAt);
